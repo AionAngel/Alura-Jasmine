dfeb99e116e33c357ce75dbf7fb0e106896839e5
AionAngel <lucas.augustus@gmail.com>
2017-12-11 11:49:54 -0200

    Adicionado o beforeEach e fim da aula 3

--- jasmine-standalone-2.0.0/spec/ConsultaSpec.js
+++ jasmine-standalone-2.0.0/spec/ConsultaSpec.js
@@ -1,17 +1,28 @@
 describe("Consulta", () => {
 
-    it("nao deve cobrar nada se for um retorno", () => {
+    var guilherme;
 
-        var guilherme = new Paciente("Guilherme", 28, 72, 1.80);
-        var consulta = new Consulta(guilherme, [], true, true);
+    beforeEach(() => {
 
-        expect(consulta.preco()).toEqual(0);
+        guilherme = new PacienteBuilder().constroi();
 
     });
 
+    describe("consultas que sao retornos", ()=> {
+
+        it("nao deve cobrar nada se for um retorno", () => {
+            
+                    var consulta = new Consulta(guilherme, [], true, true);
+            
+                    expect(consulta.preco()).toEqual(0);
+            
+                });
+
+    });
+    
+
     it("deve cobrar 25 por cada procedimento comum", () => {
 
-        var guilherme = new Paciente("Guilherme", 28, 72, 1.80);
         var consulta = new Consulta(guilherme, ["proc1", "proc2"], false, false);
 
         expect(consulta.preco()).toEqual(50);
@@ -20,7 +31,6 @@ describe("Consulta", () => {
 
     it("deve cobrar preco especifico dependendo do procedimento", () => {
 
-        var guilherme = new Paciente("Guilherme", 28, 72, 1.80);
         var consulta = new Consulta(guilherme, ["procedimento-comum", "raio-x", "procedimento-comum", "gesso"], false, false);
 
         expect(consulta.preco()).toEqual(25 + 55 + 25 + 32);
